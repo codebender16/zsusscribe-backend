955c71cf9fa4b0df7c975327850741a9bba872c4
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-07-17 14:50:17 +1000

    added netlify url in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins 'http://localhost:8080, https://susscribe.netlify.app'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+  
 end
