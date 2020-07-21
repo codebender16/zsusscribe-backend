9915f05b89bf76c079d732896d7c94ddcf03b5f2
CoderAcademy-MEL <angusl.zanshin@gmail.com>
2020-07-21 15:46:29 +1000

    rspec - set up factories, models, shouldamatchers and database cleaner

---
+++ spec/factories/custom_categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :custom_category do
+    user 
+    category
+  end
+end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :subscription do
+    sequence :name do |n| 
+      "Factory Test Trail no.#{n}" 
+    end
+    name {'Netflix'}
+    payment_amount {20}
+    payment_date {'20/07/20'}
+    reminder {'14 days'}
+    expiry_date {'22/08/20'}
+
+    association :user
+
+    trait :invalid do
+      name {nil}
+    end
+  end
+end
