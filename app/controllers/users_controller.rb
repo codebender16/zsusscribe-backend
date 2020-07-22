70e6fde16b05d7ac09a82ea41f03a777d5f56b9c
James Ma <allenbad@live.cn>
2020-07-23 09:20:38 +1000

    test with users_controller,subscription_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,30 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:update]
   def create
-    User.create(user_params)
-    render json: 'user created', status: :ok
+    user = User.new(user_params)
+    if user.save
+      render json: 'user created', status: :created
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
+  def update
+    if user_params[:email] == ''
+      current_user.password = user_params[:password]
+    else
+      current_user.email = user_params[:email]
+    end
+    render json: current_user.email, status: :ok if current_user.save
   end
-end
+  #   def create
+#     User.create(user_params)
+#     render json: 'user created', status: :ok
+#   end
+
+#   private
+
+#   def user_params
+#     params.require(:user).permit(:email, :password)
+#   end
+# end
